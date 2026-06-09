65b1c9c040a28cee635584d9deac1f47cf280b30
tatuvoardor-pixel <tatuvoardor@gmail.com>
2026-06-08 21:18:37 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='maxcore-sleep-v2';
+const CACHE='maxcore-recovery-v1';
 const FILES=['./index.html'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
